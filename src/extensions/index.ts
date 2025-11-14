4bc20c0a33618e70bd636699ad9523301314d46e
Johnny Dunn <johnnyfived@protonmail.com>
2025-11-14 13:53:26 -0800

    chore(extensions): export multi-registry types and loaders

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -3,4 +3,7 @@ export * from './events';
 export * from './ExtensionRegistry';
 export * from './ExtensionManager';
 export * from './manifest';
+export * from './RegistryConfig';
+export { MultiRegistryLoader } from './MultiRegistryLoader';
+export { ExtensionLoader } from './ExtensionLoader';
 
